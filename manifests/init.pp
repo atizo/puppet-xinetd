f5dd9d0ddc65e44f01b5923bc61f90c8ca09ac6a
Simon Josi <simon.josi@atizo.com>
2011-10-10 17:00:58 +0200

    add copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,8 @@
 # Copyright 2008, Puzzle ITC GmbH
 # Marcel Härry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
+# Copyright 2011, Atizo AG
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -12,13 +14,12 @@
 #
 
 class xinetd {
-    package{'xinetd':
-        ensure => present,
-    }
-
-    service{'xinetd':
-        ensure => running,
-        enable => true,
-        require => Package[xinetd],
-    }
+  package{'xinetd':
+    ensure => present,
+  }
+  service{'xinetd':
+    ensure => running,
+    enable => true,
+    require => Package['xinetd'],
+  }
 }
